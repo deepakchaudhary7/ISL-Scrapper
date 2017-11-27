45f6a8f96bf03b537551d06ca755cb54e1c6ca07
PRAKASH <deepak@ubuntu.ubuntu-domain>
2017-11-27 15:34:41 +0530

    Static files added

--- script.py
+++ script.py
@@ -1,84 +1,95 @@
 import requests
 import csv
-from flask import Flask, make_response, send_file
+from flask import Flask, make_response, send_file, render_template, request
 
 app = Flask(__name__)
 
 
 
 @app.route("/")
+def index():
+    return render_template('index.html')
 
+
+@app.route('/result',methods = ['POST', 'GET'])
 def Main_scrapper():
+    if request.method == 'POST':
+        return_data = request.form
+        print return_data
 
-    url         = "http://www.indiansuperleague.com/matchcentre/19963-kerala-blasters-fc-vs-atk"
-    match_no = url.split('/')[4]
-    match_no = match_no.split('-')[0]
-    Request_URL = "http://www.indiansuperleague.com/sifeeds/repo/football/live/india_sl/json/{0}.json".format(match_no)
+        for key, value in return_data.iteritems():
+            Return_URL = value.encode('utf-8')
 
-    data        = requests.get(Request_URL)
-    data        = data.json()
+        url         = Return_URL
+        match_no = url.split('/')[4]
+        match_no = match_no.split('-')[0]
+        Request_URL = "http://www.indiansuperleague.com/sifeeds/repo/football/live/india_sl/json/{0}.json".format(match_no)
+        print Request_URL
+        print "1"
+        data        = requests.get(Request_URL)
+        data        = data.json()
 
-    with open('/tmp/Data.csv', 'w') as data_file:
-        fieldnames = ['S No.', 'Season', 'Match', 'Date', 'Crowd', 'Venue', 'Team', 'Opponent', 'Result', 'Home/Away', 'Formation', 'GS', 'GA', 'GD', 'Poss.', 'Shots', 'SOT', 'Shot%', 'Pass', 'Pass%', 'PC', 'Intercept', 'Cross', 'FC', 'Offsides', 'Corners', 'YC', 'RC']
-        writer = csv.DictWriter(data_file, fieldnames=fieldnames)
-        writer.writeheader()
+        with open('/tmp/Data.csv', 'w') as data_file:
+            fieldnames = ['S No.', 'Season', 'Match', 'Date', 'Crowd', 'Venue', 'Team', 'Opponent', 'Result', 'Home/Away', 'Formation', 'GS', 'GA', 'GD', 'Poss.', 'Shots', 'SOT', 'Shot%', 'Pass', 'Pass%', 'PC', 'Intercept', 'Cross', 'FC', 'Offsides', 'Corners', 'YC', 'RC']
+            writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+            writer.writeheader()
 
-        for i in range(0,2):
-            d = {}
-            d['S No.']      =   1
-            d['Season']     =   data['match_detail']['series']['name'].split(',')[1].split('"')[0].lstrip().rstrip()
-            d['Match']      =   data["teams"][0]["name"] + "v" + data['teams'][1]["name"]
-            d['Date']       =   data['match_detail']['date']
-            d['Crowd']      =   data['match_detail']['attendance']
-            d['Venue']      =   data['match_detail']['venue']['name']
-            d['Team']       =   data['teams'][i]['name']
+            for i in range(0,2):
+                d = {}
+                d['S No.']      =   1
+                d['Season']     =   data['match_detail']['series']['name'].split(',')[1].split('"')[0].lstrip().rstrip()
+                d['Match']      =   data["teams"][0]["name"] + "v" + data['teams'][1]["name"]
+                d['Date']       =   data['match_detail']['date']
+                d['Crowd']      =   data['match_detail']['attendance']
+                d['Venue']      =   data['match_detail']['venue']['name']
+                d['Team']       =   data['teams'][i]['name']
 
-            if(i==0):
-                d['Opponent'] = data['teams'][1]['name']
-            else:
-                d['Opponent'] = data['teams'][0]['name']
-
-            if(data['match_detail']['result']['outcome']=='D'):
-                d['Result']   = "Draw"
-            else:
-                if(data['match_detail']['result']['winning_team_id']==data['teams']['id']):
-                    d['Result'] = "Won"
+                if(i==0):
+                    d['Opponent'] = data['teams'][1]['name']
                 else:
-                    d['Result'] = "Loss"
+                    d['Opponent'] = data['teams'][0]['name']
 
-            if(i==0):
-                d['Home/Away'] = "Home"
-            else:
-                d['Home/Away'] = "Away"
+                if(data['match_detail']['result']['outcome']=='D'):
+                    d['Result']   = "Draw"
+                else:
+                    if(data['match_detail']['result']['winning_team_id']==data['teams'][i]['id']):
+                        d['Result'] = "Won"
+                    else:
+                        d['Result'] = "Loss"
 
-            d['Formation']  =   data['teams'][i]['formation']
-            d['GS']         =   data['teams'][i]['score']
+                if(i==0):
+                    d['Home/Away'] = "Home"
+                else:
+                    d['Home/Away'] = "Away"
 
-            if(i==0):
-                d["GA"]     =   data['teams'][1]['score']
-            else:
-                d['GA']     =   data['teams'][0]['score']
+                d['Formation']  =   data['teams'][i]['formation']
+                d['GS']         =   data['teams'][i]['score']
 
-            d['GD']         =   int(d['GS']) - int(d['GA'])
-            d['Poss.']      =   data['teams'][i]['stats']['possession_percentage']
-            d['Shots']      =   data['teams'][i]['stats']['events']['shots']
-            d['SOT']        =   data['teams'][i]['stats']['events']['shots_on_target']
-            d['Shot%']      =   float(int(d['SOT'])*100)/(int(d['Shots']))
-            d['Pass']       =   data['teams'][i]['stats']['touches']['total_passes']
-            d['Pass%']      =   data['teams'][i]['stats']['touches']['pass_accuracy_percentage']
-            d['PC']         =   ((int(d['Pass%']) * d['Pass'])/100)
-            d['Intercept']  =   data['teams'][i]['stats']['touches']['interceptions']
-            d['Cross']      =   data['teams'][i]['stats']['events']['crosses']
-            d['FC']         =   data['teams'][i]['stats']['events']['fouls_committed']
-            d['Offsides']   =   data['teams'][i]['stats']['events']['offsides']
-            d['Corners']    =   data['teams'][i]['stats']['events']['corner_kicks']
-            d['YC']         =   data['teams'][i]['stats']['events']['yellow_cards']
-            d['RC']         =   data['teams'][i]['stats']['events']['red_cards']
+                if(i==0):
+                    d["GA"]     =   data['teams'][1]['score']
+                else:
+                    d['GA']     =   data['teams'][0]['score']
 
-            writer.writerow(d)
+                d['GD']         =   int(d['GS']) - int(d['GA'])
+                d['Poss.']      =   data['teams'][i]['stats']['possession_percentage']
+                d['Shots']      =   data['teams'][i]['stats']['events']['shots']
+                d['SOT']        =   data['teams'][i]['stats']['events']['shots_on_target']
+                d['Shot%']      =   float(int(d['SOT'])*100)/(int(d['Shots']))
+                d['Pass']       =   data['teams'][i]['stats']['touches']['total_passes']
+                d['Pass%']      =   data['teams'][i]['stats']['touches']['pass_accuracy_percentage']
+                d['PC']         =   ((int(d['Pass%']) * d['Pass'])/100)
+                d['Intercept']  =   data['teams'][i]['stats']['touches']['interceptions']
+                d['Cross']      =   data['teams'][i]['stats']['events']['crosses']
+                d['FC']         =   data['teams'][i]['stats']['events']['fouls_committed']
+                d['Offsides']   =   data['teams'][i]['stats']['events']['offsides']
+                d['Corners']    =   data['teams'][i]['stats']['events']['corner_kicks']
+                d['YC']         =   data['teams'][i]['stats']['events']['yellow_cards']
+                d['RC']         =   data['teams'][i]['stats']['events']['red_cards']
 
-    data_file.close()
-    return send_file('/tmp/Data.csv',as_attachment=True)
+                writer.writerow(d)
+
+        data_file.close()
+        return send_file('/tmp/Data.csv',as_attachment=True)
 
 
 if __name__ == "__main__":
